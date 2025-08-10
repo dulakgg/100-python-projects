041bf41b9d9a77150eb70bf77ebadd68b9795be6
Dulakgg <kubakettner1@gmail.com>
2025-08-10 11:46:27 +0200

    Rock Paper Scissors Game Added

---
+++ section_1/rock_paper_scissors.py
@@ -0,0 +1,38 @@
+import random
+
+num = random.randint(1, 3)
+player_choice = input("Choose Rock, Paper or Scissors\n>>> ").lower()
+if player_choice == "rock" or player_choice == "paper" or player_choice == "scissors":
+    match num:
+        case 1:
+            if player_choice == "paper":
+                print("Rock vs Paper")
+                print("You win!")
+            elif player_choice == "rock":
+                print("Rock vs Rock")
+                print("It's a tie")
+            else:
+                print("Rock vs Scissors")
+                print("You lose :(")
+        case 2:
+            if player_choice == "scissors":
+                print("Paper vs Scissors")
+                print("You win!")
+            elif player_choice == "paper":
+                print("Paper vs Paper")
+                print("It's a tie")
+            else:
+                print("Paper vs Scissors")
+                print("You lose :(")
+        case 3:
+            if player_choice == "rock":
+                print("Scissors vs Rock")
+                print("You win!")
+            elif player_choice == "scissors":
+                print("Scissors vs Scissors")
+                print("It's a tie")
+            else:
+                print("Scissors vs Paper")
+                print("You lose :(")
+else:
+    print("Bad decision")
